a1cf34add482ad368104f47170239cef7d8ec502
Łukasz Starosta <lstarosta98@gmail.com>
2019-11-02 13:08:54 +0100

    Add basic project with unit test

---
+++ ProgrammingTechnologies/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingTechnologies
+{
+    public class Data
+    {
+        private string name = "to dziaa bapplejens";
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
---
+++ ProgrammingTechnologiesTest/DataTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgrammingTechnologies;
+
+namespace ProgrammingTechnologiesTest
+{
+    [TestClass]
+    public class DataTest
+    {
+        [TestMethod]
+        public void TestWuja()
+        {
+            Data data = new Data();
+            Assert.AreEqual(data.getName(), "to dziaa bapplejens");
+        }
+    }
+}
