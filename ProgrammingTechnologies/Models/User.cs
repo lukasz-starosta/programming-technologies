3fcd95cf3757a8d6e8b585a8872e6954c22e4dd2
karczewskipiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2019-11-04 17:39:17 +0100

    Basic implementation of User model

---
+++ ProgrammingTechnologies/Models/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingTechnologies.Models
+{
+    class User : INotifyPropertyChanged
+    {
+        // Private fields holding values for public properties
+        private string _lastName;
+        private string _email;
+        private string _name;
+
+        // Public properties for safe exposition of data fields
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        #region INotifyPropertyChanged Implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifies about property change by raising PropertyChanged event.
+        /// </summary>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
