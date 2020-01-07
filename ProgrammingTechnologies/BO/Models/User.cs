a9760010abfe329b7388387e5d32140a78bf9608
Łukasz Starosta <lstarosta98@gmail.com>
2020-01-07 19:49:44 +0100

    User vm

--- ProgrammingTechnologies/BO/Models/User.cs
+++ ProgrammingTechnologies/BO/Models/User.cs
@@ -1,8 +1,9 @@
 ﻿using ProgrammingTechnologies.Helpers;
+using System.ComponentModel;
 
 namespace ProgrammingTechnologies.BO.Models
 {
-    public class User : Model
+    public class User : Model, IDataErrorInfo
     {
         // Private fields holding values for public properties
         private string _name;
@@ -21,6 +22,18 @@ namespace ProgrammingTechnologies.BO.Models
             {
                 _password = SecurePasswordHasher.Hash(value);
                 OnPropertyChanged("Password");
+            }
+        }
+
+        private string _readablePassword;
+        public string ReadablePassword
+        {
+            get { return _readablePassword != null ? _readablePassword : "********" ; }
+            set
+            {
+                _readablePassword = value;
+                Password = value;
+                OnPropertyChanged("ReadablePassword");
             }
         }
 
@@ -61,9 +74,117 @@ namespace ProgrammingTechnologies.BO.Models
 
         public string FullName { get { return $"{Name} {LastName}"; } }
 
+        #region IDataErrorInfoMembers
+
+        string IDataErrorInfo.Error
+        {
+            get { return null; }
+        }
+
+        string IDataErrorInfo.this[string propertyName]
+        {
+            get
+            {
+                return GetValidationError(propertyName);
+            }
+        }
+
+        #endregion
+
+        #region Validation
+        private static readonly string[] ValidatedProperties =
+        {
+            "Name",
+            "LastName",
+            "Email",
+            "ReadablePassword"
+        };
+
         public override bool isValid()
         {
+            foreach (string property in ValidatedProperties)
+            {
+                if (GetValidationError(property) != null)
+                {
+                    return false;
+                }
+            }
             return true;
         }
+
+        public string GetValidationError(string propertyName)
+        {
+            string error = null;
+
+            switch (propertyName)
+            {
+                case "Name":
+                    error = ValidateName();
+                    break;
+                case "LastName":
+                    error = ValidateLastName();
+                    break;
+                case "Email":
+                    error = ValidateEmail();
+                    break;
+                case "ReadablePassword":
+                    error = ValidatePassword();
+                    break;
+            }
+
+            return error;
+        }
+
+        private string ValidateName()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                return "Name cannot be empty.";
+            }
+            if (Name.Length > 20)
+            {
+                return "Name cannot be longer than 20 characters.";
+            }
+            return null;
+        }
+
+        private string ValidateLastName()
+        {
+            if (string.IsNullOrWhiteSpace(LastName))
+            {
+                return "Last name cannot be empty.";
+            }
+            if (LastName.Length > 40)
+            {
+                return "Last name cannot be longer than 40 characters.";
+            }
+            return null;
+        }
+        private string ValidateEmail()
+        {
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                return "Email cannot be empty.";
+            }
+            if (Email.Length > 50)
+            {
+                return "Email cannot be longer than 50 characters.";
+            }
+            return null;
+        }
+        private string ValidatePassword()
+        {
+            if (ReadablePassword.Length < 4)
+            {
+                return "Password cannot be shorter than 4 characters.";
+            }
+            if (ReadablePassword.Length > 10)
+            {
+                return "Password cannot be longer than 10 characters.";
+            }
+            return null;
+        }
+
+        #endregion
     }
 }
--- ProgrammingTechnologies/ViewModels/UserViewModel.cs
+++ ProgrammingTechnologies/ViewModels/UserViewModel.cs
@@ -1,27 +1,53 @@
-﻿using ProgrammingTechnologies.BO.Models;
+﻿using ProgrammingTechnologies.BLL.Managers;
+using ProgrammingTechnologies.BO.Models;
+using ProgrammingTechnologies.Helpers;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace ProgrammingTechnologies.ViewModels
 {
-    internal class UserViewModel : ViewModel<User>
+    internal class UserViewModel : INotifyPropertyChanged
     {
+        public string Name { get; set; }
+
+        private UserManager UserManager;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public User CurrentUser { get; set; }
         public UserViewModel()
         {
-            Name = "Users";
+            Name = "User";
+            UserManager = new UserManager(ServiceProvider.GetDatabaseDependentServices);
+            CurrentUser = UserManager.GetManagedObjectWhere("id = 135");
+
+            StartEditing = new RelayCommand(Edit, () => !IsEditing);
+            SubmitEditing = new RelayCommand(Submit, () => IsEditing && CurrentUser.isValid());
         }
 
-        protected override void AddItem()
+        private bool _isEditing;
+        public bool IsEditing { get => _isEditing; set { _isEditing = value; OnPropertyChanged("IsEditing"); } }
+
+        public ICommand StartEditing { get; private set; }
+        public ICommand SubmitEditing { get; private set; }
+
+        private void Edit()
         {
-            throw new System.NotImplementedException();
+            IsEditing = true;
         }
 
-        protected override void DeleteItem()
+        private void Submit()
         {
-            throw new System.NotImplementedException();
+            User user = CurrentUser;
+            UserManager.UpdateManagedObject(ref user);
+            CurrentUser = user;
+            IsEditing = false;
         }
 
-        protected override void UpdateItem()
+        private void OnPropertyChanged(string propertyName)
         {
-            throw new System.NotImplementedException();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
     }
 }
